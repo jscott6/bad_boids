445ef1d904a715d49c311003a77d8714a11f109d
James Scott <jamesscott@Jamess-MBP.home>
2017-01-03 00:02:35 +0000

    Refactored boids

--- bad_boids/bad_boids.py
+++ bad_boids/bad_boids.py
@@ -4,34 +4,34 @@ from matplotlib import animation
 import random
 
 # Deliberately terrible code for teaching purposes
-
-boids_x=[random.uniform(-450,50.0) for x in range(50)]
-boids_y=[random.uniform(300.0,600.0) for x in range(50)]
-boid_x_velocities=[random.uniform(0,10.0) for x in range(50)]
-boid_y_velocities=[random.uniform(-20.0,20.0) for x in range(50)]
+no_boids = 50
+boids_x=[random.uniform(-450,50.0) for x in range(no_boids)]
+boids_y=[random.uniform(300.0,600.0) for x in range(no_boids)]
+boid_x_velocities=[random.uniform(0,10.0) for x in range(no_boids)]
+boid_y_velocities=[random.uniform(-20.0,20.0) for x in range(no_boids)]
 boids=(boids_x,boids_y,boid_x_velocities,boid_y_velocities)
 
 def update_boids(boids): xs,ys,xvs,yvs=boids
-    # Fly towards the middle for i in range(50):
-    for j in range(50):
+    # Fly towards the middle for i in range(no_boids):
+    for j in range(no_boids):
         xvs[i]=xvs[i]+(xs[j]-xs[i])*0.01/len(xs)
-    for i in range(50):
-        for j in range(50):
+    for i in range(no_boids):
+        for j in range(no_boids):
             yvs[i]=yvs[i]+(ys[j]-ys[i])*0.01/len(xs)
     # Fly away from nearby boids
-    for i in range(50):
-        for j in range(50):
+    for i in range(no_boids):
+        for j in range(no_boids):
                 if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 100:
                      xvs[i]=xvs[i]+(xs[i]-xs[j])
                      yvs[i]=yvs[i]+(ys[i]-ys[j])
     # Try to match speed with nearby boids
-    for i in range(50):
-        for j in range(50):
+    for i in range(no_boids):
+        for j in range(no_boids):
             if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 10000:
                 xvs[i]=xvs[i]+(xvs[j]-xvs[i])*0.125/len(xs)
                 yvs[i]=yvs[i]+(yvs[j]-yvs[i])*0.125/len(xs)
     # Move according to velocities
-    for i in range(50):
+    for i in range(no_boids):
         xs[i]=xs[i]+xvs[i]
         ys[i]=ys[i]+yvs[i]
 
