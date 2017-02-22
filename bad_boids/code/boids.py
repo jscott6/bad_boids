263745a02e060999beef283ca45078afd58ac6f7
James Scott <jamesscott@jamess-mbp.home>
2017-02-22 13:16:28 +0000

    Fixed View and Controller classes

--- bad_boids/code/boids.py
+++ bad_boids/code/boids.py
@@ -1,14 +1,9 @@
 
-from matplotlib import pyplot as plt
-from matplotlib import animation
-import numpy as np
-import yaml
-from os.path import join, dirname
-from .funcs import initiate_array, middle, calc_distances, avoid_boids, match_boids
+from .funcs import *
 
 # create a boids class
 
-class boids(object):
+class Boids(object):
 
     def __init__(self, size = 50):
         self.size = size
@@ -31,25 +26,3 @@ class boids(object):
 
         ''' ___UPDATE POSITIONS___'''
         self.positions += self.velocities
-
-
-config = yaml.load(open(join(dirname(__file__), 'config.yaml')))
-# instantiate a flock with appropriate parameters
-flock = boids(50)
-# initiate position and velocities of boids
-flock.initiate(config['init_data'])
-# update the position and velocities
-flock.update(config['params'])
-
-figure=plt.figure()
-axes=plt.axes(xlim=(-500,1500), ylim=(-500,1500))
-scatter=axes.scatter(flock.positions[0],flock.positions[1])
-
-def animate(frame):
-    flock.update(config['params'])
-    scatter.set_offsets((flock.positions[0],flock.positions[1]))
-
-anim = animation.FuncAnimation(figure, animate, frames=200, interval=50)
-
-if __name__ == "__main__":
-    plt.show()
--- bad_boids/code/view.py
+++ bad_boids/code/view.py
@@ -3,11 +3,11 @@
 class View(object):
     def __init__(self, flock):
         from matplotlib import pyplot as plt
-        self.figure = plt.figure()
-        axes = plt.axes()
+        self.fig = plt.figure()
+        axes = plt.axes(xlim = (-500,1500), ylim = (-500,1500))
         self.flock = flock
-        self.scatter = axes.scatter(flock.positions[0], flock.positions[1])
+        self.scatter = axes.scatter(self.flock.positions[0], self.flock.positions[1])
 
 
     def update(self):
-        self.scatter.set_offsets((flock.positions[0], flock.positions[1]))
+        self.scatter.set_offsets((self.flock.positions[0], self.flock.positions[1]))
